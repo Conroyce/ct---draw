cbab65ec30e79acfece3e4b4c649e2c5e860da5c
Connor Kojimoto <cokojimoto@gmail.com>
2015-04-16 21:29:54 -0700

    removes dispatcher bind and converts rails - angular communication to only use trigger

--- app/controllers/draw_controller.rb
+++ app/controllers/draw_controller.rb
@@ -1,17 +1,14 @@
 class DrawController < WebsocketRails::BaseController
   def initialize_session
-    controller_store[:message_count] = 0
+    controller_store[:x] = []
+    controller_store[:y] = []
+    controller_store[:drag] = []
   end  
 
   def create
-    binding.pry
-    # draw = Draw.new message
-    new_message = {:message => 'this is a message'}
-    send_message :returnDraw, new_message
-    # if draw.save
-    #   send_message :create_success, draw
-    # else 
-    #   send_message :create, new_message 
-    # end   
+    controller_store[:x].push(message[:x])
+    controller_store[:y].push(message[:y])
+    controller_store[:drag].push(message[:drag])
+    trigger_success [controller_store[:x],controller_store[:y],controller_store[:drag]]
   end  
 end
